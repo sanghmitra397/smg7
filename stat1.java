27d1bfe0472a5619762c9ab89aafcb1d7a508eb0
sanghmitra397 <120152756+sanghmitra397@users.noreply.github.com>
2022-12-19 10:12:35 +0530

    Add files via upload

---
+++ stat1.java
@@ -0,0 +1,26 @@
+class stat1 
+{
+	 static double d;
+		System.out.println(d);
+	
+	public static void main(String[] args) 
+	{
+		
+		
+		double d='s';
+		System.out.println(stat1.d);
+		System.out.println(d);
+		
+		System.out.println(demo());
+	}
+		public static String demo()
+	{
+		System.out.println(d);
+		String d="hello";
+		System.out.println(stat1.d);
+		return d;
+	}
+		//System.out.println(d);
+
+
+}
